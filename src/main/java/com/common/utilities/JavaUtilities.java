4310ede23c9df4ed2e34aa878a3687cd530bf120
Lella-PC\Lella <muralilella@gmail.com>
2017-07-15 14:37:04 -0400

    Added new utility

--- src/main/java/com/common/utilities/JavaUtilities.java
+++ src/main/java/com/common/utilities/JavaUtilities.java
@@ -4,20 +4,18 @@ import java.util.List;
 
 public class JavaUtilities {
 
-	/*public static void main(String[] args){
-		JavaUtilities javaUtilities = new JavaUtilities();
-		List<String> myList =
-			    Arrays.asList("a1", "a2", "b1", "c2", "c1");
-		javaUtilities.printArray(myList);
-	}*/
+	/*
+	 * public static void main(String[] args){ JavaUtilities javaUtilities = new
+	 * JavaUtilities(); List<String> myList = Arrays.asList("a1", "a2", "b1",
+	 * "c2", "c1"); javaUtilities.printArray(myList); }
+	 */
 
-	public void printArray(List<String> myList){
-			myList
-			    .stream()
-			    .filter(s -> s.startsWith("c"))
-			    .map(String::toUpperCase)
-			    .sorted()
-			    .forEach(System.out::println);
+	public void printArray(List<String> myList) {
+		myList.stream().filter(s -> s.startsWith("c")).map(String::toUpperCase).sorted().forEach(System.out::println);
 	}
-	
+
+	public String[] stringToArray(String array, String splitChar) {
+		return  array.split(splitChar);
+	}
+
 }
--- src/test/java/test/common/utilities/JavaUtilitiesTest.java
+++ src/test/java/test/common/utilities/JavaUtilitiesTest.java
@@ -3,6 +3,7 @@ package test.common.utilities;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.common.utilities.JavaUtilities;
@@ -17,4 +18,12 @@ public class JavaUtilitiesTest {
 		javaUtilities.printArray(myList);
 	}
 	
+	@Test
+	public void stringToArrayTest(){
+		JavaUtilities javaUtilities = new JavaUtilities();
+		String astr = "string1,String2";
+		String[] expected = {"string1", "String2"};
+		String[] result = javaUtilities.stringToArray(astr, ",");
+		Assert.assertArrayEquals(expected, result);
+	}
 }
